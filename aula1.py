e68277144af37532a9092bd8b9660f0114556875
Matheus Magano Bueno <142444039+MatheusMaganoBueno@users.noreply.github.com>
2024-11-20 16:52:39 -0300

    Initial commit

---
+++ aula1.py
@@ -0,0 +1,16 @@
+"""
+DocString
+E Escrever o que eu
+quiser
+
+
+auwfwbfwbf
+"""
+'''
+Usar para escrever suas notas
+'''
+
+# Permite escrever um comentário
+print(123) # Na frente
+# Abaixo
+print(456) 
---
+++ aula3.py
@@ -0,0 +1,20 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos  que estão dentro de aspas
+"""
+
+# Aspas simples 
+print('Matheus Bueno')
+print(1,'Matheus "Bueno"')
+
+# Aspas duplas 
+print("Matheus Bueno")
+print(2, "Matheus 'Bueno'")
+
+# Escape
+print("Matheus \"Bueno\"")
+
+# r
+print(r"Matheus \"Bueno\"")
